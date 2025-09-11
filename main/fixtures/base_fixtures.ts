e8a766fd20489d3bbfb88b7ea287cacd2fa53402
vitaliyakob <vitalii_yakob@pecodesoftware.com>
2025-09-11 10:30:08 +0300

    try now

--- main/fixtures/base_fixtures.ts
+++ main/fixtures/base_fixtures.ts
@@ -2,7 +2,7 @@ import BasePage from "../pages/BasePage";
 import CalendarPage from "../pages/CalendarPage";
 import DetailsPage from "../pages/DetailsPage";
 import LoginPage from "../pages/LoginPage"
-import {test as base} from '@playwright/test'
+import { test as base } from '@playwright/test'
 import MainPage from "../pages/MainPage";
 import AudubonsPage from "../pages/AudubonsPage";
 import PaymentPage from "../pages/PaymentPage";
@@ -20,39 +20,69 @@ type Fixtures = {
     paymentPage: PaymentPage;
     receiptPage: ReceiptPage;
     membershipPage: MembershipPage;
-    familyPage: FamilyPage
-};
+    familyPage: FamilyPage;
+}
 
 export const test = base.extend<Fixtures>({
-    loginPage: async ({page}, use) => {
+    loginPage: async ({ browser }, use) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
         await use(new LoginPage(page));
+        await context.close();
     },
-    basePage: async ({page}, use) => {
+    basePage: async ({ browser }, use) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
         await use(new BasePage(page));
+        await context.close();
     },
-    mainPage: async ({page}, use) => {
+    mainPage: async ({ browser }, use) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
         await use(new MainPage(page));
+        await context.close();
     },
-    detailsPage: async ({page}, use) => {
+    detailsPage: async ({ browser }, use) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
         await use(new DetailsPage(page));
+        await context.close();
     },
-    calendarPage: async ({page}, use) => {
+    calendarPage: async ({ browser }, use) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
         await use(new CalendarPage(page));
+        await context.close();
     },
-    paymentPage: async ({page}, use) => {
+    paymentPage: async ({ browser }, use) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
         await use(new PaymentPage(page));
+        await context.close();
     },
-    audubonPage: async ({page}, use) => {
+    audubonPage: async ({ browser }, use) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
         await use(new AudubonsPage(page));
+        await context.close();
     },
-    receiptPage: async ({page}, use) => {
+    receiptPage: async ({ browser }, use) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
         await use(new ReceiptPage(page));
+        await context.close();
     },
-    membershipPage: async ({page}, use) => {
+    membershipPage: async ({ browser }, use) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
         await use(new MembershipPage(page));
+        await context.close();
     },
-    familyPage: async ({page}, use) => {
+    familyPage: async ({ browser }, use) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
         await use(new FamilyPage(page));
+        await context.close();
     },
 })
 
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,64 +1,68 @@
 import { expect, test } from "../main/fixtures/base_fixtures";
 import { password, testUsers } from '../main/config/users';
-import { getFreshchatToken, getMembershipId, getSessionKey, getSessionKeyFirst, getWpNonce } from "../main/api/apiHelper";
+import { getFreshchatToken, getMembershipId, getSessionKey, getWpNonce } from "../main/api/apiHelper";
 
-test.describe.parallel('Login Page', () => {
-  for (const user of testUsers) {
-    test(`add items and make payments for ${user.email}`, async ({
-      basePage,
-      loginPage,
-      mainPage,
-      detailsPage,
-      paymentPage,
-      receiptPage,
-      membershipPage,
-      familyPage,
-      page,
-      request
-    }, testInfo) => {
-      console.log(`Running test with user: ${user.email}`);
-      await basePage.navigateToURL();
-      await basePage.loginToApplication(password);
-      await mainPage.addPromoCode("ANNUAL15");
-      await mainPage.navigateToMembershipsLevel();
-      await membershipPage.clickMembership(user.productName);
-      // await familyPage.expectRedirect('memberships/family/');
-      await familyPage.addFamilyToCard();
-       const nonce = await getWpNonce(page);
-      const sessionKey = await getSessionKey(request, nonce);
-      const membershipId = await getMembershipId(request, nonce);
-      console.log(sessionKey, user.email);
-      console.log(membershipId)
-      const attachmentData = Buffer.from(JSON.stringify({ sessionKey, membershipId }, null, 2));
+const USERS_PER_BLOCK = 10; 
 
-      testInfo.attachments.push({
-        name: `Session and Membership for ${user.email}`,
-        contentType: 'application/json',
-        body: attachmentData,
+
+for (let i = 0; i < testUsers.length; i += USERS_PER_BLOCK) {
+  const chunk = testUsers.slice(i, i + USERS_PER_BLOCK);
+
+  test.describe.parallel(`User block ${i / USERS_PER_BLOCK + 1}`, () => {
+    for (const user of chunk) {
+      test(`add items and make payments for ${user.email}`, async ({
+        basePage,
+        loginPage,
+        mainPage,
+        detailsPage,
+        paymentPage,
+        receiptPage,
+        membershipPage,
+        familyPage,
+        page,
+        request
+      }, testInfo) => {
+        console.log(`Running test with user: ${user.email}`);
+
+        // Ізольована сесія для кожного користувача
+        await basePage.navigateToURL();
+        await basePage.loginToApplication(password);
+
+        await mainPage.addPromoCode("ANNUAL15");
+        await mainPage.navigateToMembershipsLevel();
+        await membershipPage.clickMembership(user.productName);
+        await familyPage.addFamilyToCard();
+
+        const nonce = await getWpNonce(page);
+        const sessionKey = await getSessionKey(request, nonce);
+        const membershipId = await getMembershipId(request, nonce);
+
+        console.log(sessionKey, user.email);
+        console.log(membershipId);
+
+        const attachmentData = Buffer.from(JSON.stringify({ sessionKey, membershipId }, null, 2));
+
+        testInfo.attachments.push({
+          name: `Session and Membership for ${user.email}`,
+          contentType: 'application/json',
+          body: attachmentData,
+        });
+
+        await mainPage.goToCart();
+        await detailsPage.checkNameOfTheAddedItem(user.productName);
+        await detailsPage.checkoutTOPayments();
+        await loginPage.submitLoginForm(user.email, user.password);
+
+        const billing = await detailsPage.getBillingText();
+        await detailsPage.checkNameOfTheAddedItem(user.productName);
+
+        await paymentPage.fillAdyenCard('3607 0500 0010 20','03/30','737','US');
+        await paymentPage.agreeToTheTerms();
+        await paymentPage.completeCheckout();
+
+        await receiptPage.expectBillingTextToContain(billing);
+        await receiptPage.getNameItem(user.productName);
       });
-      await mainPage.goToCart();
-      await detailsPage.checkNameOfTheAddedItem(user.productName);
-      
-      await detailsPage.checkoutTOPayments();
-      await loginPage.submitLoginForm(user.email, user.password);
-      // const token = await getFreshchatToken(page);
-      // console.log('Freshchat token:', token);
-      
-      
-      // await detailsPage.checkMessage();
-
-      const billing = await detailsPage.getBillingText();
-      await detailsPage.checkNameOfTheAddedItem(user.productName);
-      await paymentPage.fillAdyenCard('3607 0500 0010 20','03/30','737','US');
-      await paymentPage.agreeToTheTerms();
-      await paymentPage.completeCheckout();
-      // await receiptPage.expectRedirect('cart/receipt/');
-      // await receiptPage.expectOrderTextToContain("");
-      await receiptPage.expectBillingTextToContain(billing);
-      await receiptPage.getNameItem(user.productName);
-      // const tokenAfterBuy = await getFreshchatToken(page);
-      // console.log('Freshchat token:', tokenAfterBuy);
-      // await expect(token).toEqual(tokenAfterBuy)
-    });
-  }
-});
+    }
+  });
+}
