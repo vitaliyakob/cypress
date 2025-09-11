e31d1afba4319c8b0a9fd84a0c6d94948a8a466c
vitaliyakob <vitalii_yakob@pecodesoftware.com>
2025-09-11 13:24:54 +0300

    add workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   timeout: 400000,
-  workers: 5,
+  workers: 14,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 0 : 0,
   reporter: [
